663d3c75341a75117d6ec6b95a02539558812861
Monsieur <christopher.chhim123@gmail.com>
2024-09-25 18:15:59 -0700

    Initiated main.jsx file

---
+++ client/src/main.jsx
@@ -0,0 +1,45 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+
+import App from './App.jsx'
+import Home from './pages/Home';
+import Post from './pages/Post';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Recipe from './pages/Recipe';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NoMatch />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        }, {
+          path: '/login',
+          element: <Login />
+        }, {
+          path: '/signup',
+          element: <Signup />
+        }, {
+          path: '/profile',
+          element: <Profile />
+          },{
+          path: '/recipes/:id',
+          element: <Recipe />
+        }, {
+          path: '/post',
+          element: <Post />
+        }
+      ]
+    }
+  ])
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+  )
+  
