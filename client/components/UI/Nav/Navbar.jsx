5c9e3fe5b4025aa1fc99b8bf891b4c4649e7b96c
Monsieur <christopher.chhim123@gmail.com>
2024-09-26 17:15:39 -0700

    Finished navbar

---
+++ client/components/UI/Nav/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Nav({ links }) {
+    return (
+      <nav className="navbar navbar-expand-lg bg-secondary">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {links.map((link) => link)}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
