9e1f343bafd3ceccdaceb97e189583a28e2ff502
sofia19999 <kaminskisofia9@gmail.com>
2024-10-02 15:28:02 -0400

    new changes

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Recipe = require('./Recipe');
 const commentSchema = require('./Comment');
 
